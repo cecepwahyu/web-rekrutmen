762af976e9edbca25a78e0b6c7bb913576352ebd
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-27 08:05:27 +0700

    fix: adjust layout of copyright section in footer for better responsiveness

--- src/components/FooterCopyright.tsx
+++ src/components/FooterCopyright.tsx
@@ -6,7 +6,7 @@ const FooterCopyright = () => {
     const currentYear = new Date().getFullYear();
     return (
         <div className="flex relative z-10 bg-darkBlue h-10 items-center">
-            <div className="w-1/2 px-4 text-white flex items-center text-xs ml-32">
+            <div className="w-full px-4 text-white flex items-center text-xs ml-4 md:ml-32">
                 &copy; {currentYear} BANK BPD DIY. All Rights Reserved.
             </div>
         </div>
