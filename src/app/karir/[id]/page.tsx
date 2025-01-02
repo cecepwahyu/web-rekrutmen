3aa561b95313965e502d4eb8f0bc0e8db221568c
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2025-01-02 11:34:03 +0700

    fix: enhance error handling and optimize performance in user authentication flow

--- src/app/karir/[id]/page.tsx
+++ src/app/karir/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendar, faTag } from '@fortawesome/free-solid-svg-icons';
@@ -14,6 +14,7 @@ import CariKarirButton from '../../../components/CariKarirButton';
 import LottieAnimation from '../../../components/Animations';
 import loadingAnimation from '../../../../public/animations/loading.json';
 import animation404 from '../../../../public/animations/404.json';
+import { toast } from 'sonner';
 
 interface Article {
     judulLowongan: string;
@@ -32,12 +33,44 @@ interface Tahapan {
     isActive: boolean;
 }
 
+const getIdFromToken = async (token: string): Promise<string | null> => {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/get-id-peserta`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+                Accept: "application/json",
+            },
+        });
+
+        const data = await response.json();
+        if (data.responseCode === "000") {
+            return data.data.idPeserta || null;
+        } else {
+            console.error("Error fetching ID:", data.responseMessage);
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching ID:", error);
+        return null;
+    }
+};
+
 const DetailKarir = () => {
     const params = useParams();
-    const slug = params?.id as string; // Get slug from URL
+    const slug = params?.id as string;
     const [isLoading, setIsLoading] = useState(true);
     const [article, setArticle] = useState<Article | null>(null);
     const [tahapan, setTahapan] = useState<Tahapan[]>([]);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isLocked, setIsLocked] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        setIsAuthenticated(!!token);
+    }, []);
 
     useEffect(() => {
         const fetchArticle = async () => {
@@ -97,14 +130,78 @@ const DetailKarir = () => {
         fetchTahapan();
     }, [slug]);
 
+    useEffect(() => {
+        const fetchLockStatus = async () => {
+            const token = localStorage.getItem('token');
+            if (!token) return;
+
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/lowongan/lock-status/45`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                        Accept: 'application/json',
+                    },
+                });
+
+                const data = await response.json();
+                if (data.responseCode === '000') {
+                    setIsLocked(data.data === 'true');
+                }
+            } catch (error) {
+                console.error('Error fetching lock status:', error);
+            }
+        };
+
+        fetchLockStatus();
+    }, []);
+
+    const handleApply = async () => {
+        const token = localStorage.getItem('token');
+        if (!token) return;
+
+        const idPeserta = await getIdFromToken(token);
+        if (!idPeserta) return;
+
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/lowongan/slug/${slug}/apply`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                    Accept: 'application/json',
+                },
+                body: JSON.stringify({ id_peserta: idPeserta }),
+            });
+
+            const data = await response.json();
+            if (data.responseCode === '000') {
+                toast.success("Berhasil mendaftar!", { style: { backgroundColor: 'white', color: 'green' } });
+                setTimeout(() => {
+                    router.push('/karir');
+                }, 3000);
+            } else {
+                toast.error("Gagal menyimpan data: " + data.message, { style: { backgroundColor: 'white', color: 'red' } });
+            }
+        } catch (error) {
+            console.error('Error applying:', error);
+            alert('An error occurred. Please try again.');
+        }
+    };
+
+    if (!isAuthenticated) {
+        return null; // Return null during SSR to avoid hydration mismatch
+    }
+
     return (
         <div className="min-h-screen bg-gray-100 font-sans relative">
             <MenuBar />
-            <main className="pt-28 bg-gradient-to-r from-[#015CAC] to-[#018ED2] relative z-10">
+            <div className="pt-28 bg-gradient-to-r from-[#015CAC] to-[#018ED2] relative z-10">
                 <div className="bg-white relative z-10">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                         <defs>
-                            <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                            <linearGradient id="grad1" x1="0%" y1="0%" x2="100%">
                                 <stop offset="0%" style={{ stopColor: '#015CAC', stopOpacity: 1 }} />
                                 <stop offset="100%" style={{ stopColor: '#018ED2', stopOpacity: 1 }} />
                             </linearGradient>
@@ -115,8 +212,9 @@ const DetailKarir = () => {
                         ></path>
                     </svg>
                 </div>
-
-                <div className="flex flex-col justify-center items-center w-full bg-white h-min-[400px] relative z-10 -mt-32">
+            </div>
+            <main className="relative z-10 -mt-32">
+                <div className="flex flex-col justify-center items-center w-full bg-white h-min-[400px] relative z-10">
                     {isLoading ? (
                         <div className="flex justify-center items-center mt-10">
                             <LottieAnimation animationData={loadingAnimation} />
@@ -151,21 +249,53 @@ const DetailKarir = () => {
                                             </div>
                                         </div>
                                         <div className="flex justify-center mt-4 space-x-2">
-                                            <a href="#" className="text-blue-500" title="Share on Facebook">
+                                            <a
+                                                href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`}
+                                                className="text-blue-500"
+                                                title="Share on Facebook"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
                                                 <FontAwesomeIcon icon={faFacebookF} />
                                             </a>
-                                            <a href="#" className="text-green-500" title="Share on WhatsApp">
+                                            <a
+                                                href={`https://api.whatsapp.com/send?text=${encodeURIComponent(window.location.href)}`}
+                                                className="text-green-500"
+                                                title="Share on WhatsApp"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
                                                 <FontAwesomeIcon icon={faWhatsapp} />
                                             </a>
-                                            <a href="#" className="text-blue-400" title="Share on Twitter">
+                                            <a
+                                                href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(window.location.href)}`}
+                                                className="text-blue-400"
+                                                title="Share on Twitter"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
                                                 <FontAwesomeIcon icon={faTwitter} />
                                             </a>
-                                            <a href="#" className="text-blue-700" title="Share on LinkedIn">
+                                            <a
+                                                href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(window.location.href)}`}
+                                                className="text-blue-700"
+                                                title="Share on LinkedIn"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
                                                 <FontAwesomeIcon icon={faLinkedinIn} />
                                             </a>
-                                            {/* <button onClick={() => navigator.clipboard.writeText(window.location.href)} className="text-gray-500" title="Copy Link">
-                                                <FontAwesomeIcon icon={faLink} />
-                                            </button> */}
+                                        </div>
+                                        <div className="flex justify-center mt-8">
+                                            <button
+                                                className={`py-2 px-6 rounded-lg shadow-lg transition duration-300 transform ${
+                                                    isLocked ? 'bg-gray-200 text-gray-500 cursor-not-allowed' : 'bg-darkBlue text-white hover:bg-blue-700 hover:scale-105'
+                                                }`}
+                                                onClick={isLocked ? undefined : handleApply}
+                                                disabled={isLocked}
+                                            >
+                                                {isLocked ? 'Anda telah mendaftar' : 'Daftar'}
+                                            </button>
                                         </div>
                                         <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-4">
                                             <div className="col-span-2">
@@ -174,25 +304,17 @@ const DetailKarir = () => {
                                                 <h2 className="font-semibold text-lg mt-6">Persyaratan</h2>
                                                 <p dangerouslySetInnerHTML={{ __html: article.persyaratan }} />
                                             </div>
-                                            <div className="bg-gray-100 p-4 rounded-lg">
+                                            <div className="bg-gray-100 p-4 rounded-lg md:sticky md:top-28">
                                                 <h2 className="font-semibold text-lg">Periode Pendaftaran</h2>
                                                 <p>
                                                     <span>
-                                                        {new Date(article.periodeAwal).toLocaleDateString('id-ID', { day: 'numeric', month: 'long' })} s/d{' '}
-                                                        {new Date(article.periodeAkhir).toLocaleDateString('id-ID', { day: 'numeric', month: 'long' })}{' '}
-                                                        {new Date(article.periodeAwal).getFullYear() === new Date(article.periodeAkhir).getFullYear() ? 
-                                                            ` ${new Date(article.periodeAwal).getFullYear()}` : 
-                                                            ` ${new Date(article.periodeAwal).getFullYear()} - ${new Date(article.periodeAkhir).getFullYear()}`}
+                                                        {new Date(article.periodeAwal).toLocaleDateString('id-ID', { day: 'numeric', month: 'long', year: new Date(article.periodeAwal).getFullYear() !== new Date(article.periodeAkhir).getFullYear() ? 'numeric' : undefined })} s/d{' '}
+                                                        {new Date(article.periodeAkhir).toLocaleDateString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' })}
                                                     </span>
                                                 </p>
                                                 <h2 className="font-semibold text-lg mt-4">Lokasi Tes</h2>
                                                 <p>Yogyakarta</p>
                                             </div>
-                                        </div>
-                                        <div className="flex justify-center mt-8">
-                                            <button className="bg-darkBlue text-white py-2 px-6 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300 transform hover:scale-105">
-                                                Daftar
-                                            </button>
                                         </div>
                                     </div>
                                 </div>
