fb828f89c5739bbe7cbbbd6495ce08c6de00dfd7
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2025-01-03 10:25:19 +0700

    feat: display default message when no job application is found

--- src/app/riwayat/page.tsx
+++ src/app/riwayat/page.tsx
@@ -79,9 +79,9 @@ const Riwayat = () => {
           const profileData = await profileResponse.json();
           if (profileData.responseCode === '000') {
             setApplicantData({
-              nama: profileData.data.nama,
-              nomorPeserta: profileData.data.lowonganId,
-              posisi: profileData.data.judulLowongan,
+              nama: profileData.data.nama || "Tidak ada lamaran",
+              nomorPeserta: profileData.data.lowonganId || "Tidak ada lamaran",
+              posisi: profileData.data.judulLowongan || "Tidak ada lamaran",
             });
           }
         }
@@ -148,60 +148,68 @@ const Riwayat = () => {
           </svg>
         </div>
 
-        <div className="flex flex-col justify-center items-center w-full bg-white min-h-[400px] relative z-10 -mt-32 pb-10">
+        {applicantData.nama === "Tidak ada lamaran" ? (
+          <div className="flex flex-col justify-center items-center w-full bg-white min-h-[400px] relative z-10 -mt-32 pb-10">
             <h2 className="text-3xl font-bold text-darkBlue mb-6 text-center pt-6 sm:pt-0">
-            Tahapan Rekrutmen
+              Anda belum melamar pekerjaan
+            </h2>
+          </div>
+        ) : (
+          <div className="flex flex-col justify-center items-center w-full bg-white min-h-[400px] relative z-10 -mt-32 pb-10">
+            <h2 className="text-3xl font-bold text-darkBlue mb-6 text-center pt-6 sm:pt-0">
+              Tahapan Rekrutmen
             </h2>
 
-          {/* Rectangle Container */}
-          <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-4xl mb-10">
-            <div className="flex flex-col sm:flex-row sm:justify-between text-sm sm:text-base">
-              <p><span className="font-semibold">Nama:</span> {applicantData.nama}</p>
-              <p><span className="font-semibold">Nomor Peserta:</span> {applicantData.nomorPeserta}</p>
-              <p><span className="font-semibold">Posisi Dilamar:</span> {applicantData.posisi}</p>
+            {/* Rectangle Container */}
+            <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-4xl mb-10">
+              <div className="flex flex-col sm:flex-row sm:justify-between text-sm sm:text-base">
+                <p><span className="font-semibold">Nama:</span> {applicantData.nama}</p>
+                <p><span className="font-semibold">Nomor Peserta:</span> {applicantData.nomorPeserta}</p>
+                <p><span className="font-semibold">Posisi Dilamar:</span> {applicantData.posisi}</p>
+              </div>
+            </div>
+
+            {/* Progress Bar Container */}
+            <div className="relative flex flex-col sm:flex-row items-center w-full max-w-4xl px-4">
+              <ol className="border-s border-neutral-300 dark:border-neutral-500 md:flex md:gap-6 md:border-s-0 md:border-t-2">
+                {steps.map((step, index) => (
+                  <li key={step.idTahapan} className="flex-1">
+                    <div className="flex-start flex items-center pt-2 md:block md:pt-0">
+                      <div className={`-ms-[22px] me-3 w-10 h-10 flex items-center justify-center rounded-full ${passedSteps.includes(index + 1) ? 'bg-green-600 border-green-500' : 'bg-gray-300 border-gray-100'} md:-mt-[22px] md:me-0 md:ms-0`}>
+                        <span className="text-white">{index + 1}</span>
+                      </div>
+                      <div>
+                        <h4 className={`md:mt-2 mb-1.5 ${passedSteps.includes(index + 1) ? 'text-green-600' : 'text-gray-700'} font-medium`}>{step.namaTahapan}</h4>
+                        {passedSteps.includes(index + 1) && (
+                          <span className="py-1 px-2 inline-block bg-green-100 text-green-600 font-semibold text-xs rounded-lg">Lolos</span>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            </div>
+
+            {/* Rectangle Container */}
+            <div className="bg-blue-50 rounded-lg p-6 w-full max-w-4xl mt-28 border-2 border-darkBlue border-dashed mb-10">
+              <div className="text-sm sm:text-base">
+                {/* Heading */}
+                <p className="font-semibold text-darkBlue text-lg mb-4">Informasi Test</p>
+
+                {/* Details */}
+                <p className="mb-2">Bagi pendaftar yang dinyatakan <span className="font-semibold bg-green-100 text-green-600">LOLOS</span> diharapkan datang di:</p>
+                <p className="mb-1"><span className="font-semibold">Hari/Tanggal:</span> [Isi Hari/Tanggal]</p>
+                <p className="mb-1"><span className="font-semibold">Tempat:</span> [Isi Tempat]</p>
+                <p className="mb-3"><span className="font-semibold">Waktu:</span> [Isi Waktu]</p>
+                <p className="mb-4">Bagi pendaftar yang <span className="font-semibold bg-green-100 text-green-600">LOLOS</span> Seleksi Tes Praktik Teknologi Informasi akan diberitahukan melalui Website PT Bank BPD DIY pada tanggal <span className="font-semibold">06 Desember 2024 Pukul 16.00 WIB.</span></p>
+                <p className="mb-4"><span className="font-semibold">Catatan: Pendaftar tidak diperkenankan menggunakan kendaraan roda 4 mengingat terbatasnya area parkir.</span></p>
+                <p className="mb-4">Demikian, Terima kasih.</p>
+                <p className="mb-8"><span className="font-semibold">Yogyakarta, 30 November 2024</span></p>
+                <p className="mb-8"><span className="font-semibold">Tim Penerimaan Pegawai</span></p>
+              </div>
             </div>
           </div>
-
-          {/* Progress Bar Container */}
-          <div className="relative flex flex-col sm:flex-row items-center w-full max-w-4xl px-4">
-            <ol className="border-s border-neutral-300 dark:border-neutral-500 md:flex md:gap-6 md:border-s-0 md:border-t-2">
-              {steps.map((step, index) => (
-                <li key={step.idTahapan} className="flex-1">
-                  <div className="flex-start flex items-center pt-2 md:block md:pt-0">
-                    <div className={`-ms-[22px] me-3 w-10 h-10 flex items-center justify-center rounded-full ${passedSteps.includes(index + 1) ? 'bg-green-600 border-green-500' : 'bg-gray-300 border-gray-100'} md:-mt-[22px] md:me-0 md:ms-0`}>
-                      <span className="text-white">{index + 1}</span>
-                    </div>
-                    <div>
-                      <h4 className={`md:mt-2 mb-1.5 ${passedSteps.includes(index + 1) ? 'text-green-600' : 'text-gray-700'} font-medium`}>{step.namaTahapan}</h4>
-                      {passedSteps.includes(index + 1) && (
-                        <span className="py-1 px-2 inline-block bg-green-100 text-green-600 font-semibold text-xs rounded-lg">Lolos</span>
-                      )}
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ol>
-          </div>
-
-          {/* Rectangle Container */}
-          <div className="bg-blue-50 rounded-lg p-6 w-full max-w-4xl mt-28 border-2 border-darkBlue border-dashed mb-10">
-            <div className="text-sm sm:text-base">
-              {/* Heading */}
-              <p className="font-semibold text-darkBlue text-lg mb-4">Informasi Test</p>
-
-              {/* Details */}
-              <p className="mb-2">Bagi pendaftar yang dinyatakan <span className="font-semibold bg-green-100 text-green-600">LOLOS</span> diharapkan datang di:</p>
-              <p className="mb-1"><span className="font-semibold">Hari/Tanggal:</span> [Isi Hari/Tanggal]</p>
-              <p className="mb-1"><span className="font-semibold">Tempat:</span> [Isi Tempat]</p>
-              <p className="mb-3"><span className="font-semibold">Waktu:</span> [Isi Waktu]</p>
-              <p className="mb-4">Bagi pendaftar yang <span className="font-semibold bg-green-100 text-green-600">LOLOS</span> Seleksi Tes Praktik Teknologi Informasi akan diberitahukan melalui Website PT Bank BPD DIY pada tanggal <span className="font-semibold">06 Desember 2024 Pukul 16.00 WIB.</span></p>
-              <p className="mb-4"><span className="font-semibold">Catatan: Pendaftar tidak diperkenankan menggunakan kendaraan roda 4 mengingat terbatasnya area parkir.</span></p>
-              <p className="mb-4">Demikian, Terima kasih.</p>
-              <p className="mb-8"><span className="font-semibold">Yogyakarta, 30 November 2024</span></p>
-              <p className="mb-8"><span className="font-semibold">Tim Penerimaan Pegawai</span></p>
-            </div>
-          </div>
-        </div>
+        )}
 
         <FooterCopyright />
         <ScrollToTopButton />
