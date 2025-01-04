02a72106afe7b515408406e545e7ed4d4f27fa50
Cecep Wahyu Cahyana <cecepwahyu.2019@student.uny.ac.id>
2025-01-04 20:48:45 +0700

    feat: add profile and logout buttons to DesktopNavLinks component

--- src/components/DesktopNavLinks.tsx
+++ src/components/DesktopNavLinks.tsx
@@ -231,6 +231,17 @@ function DesktopNavLinks() {
                     className="block w-full text-left px-4 py-2 text-darkBlue hover:bg-gray-100 hover:rounded-md flex items-center"
                     onClick={() => handleProfileClick('/profil')}
                   >
+                    <FontAwesomeIcon icon={faUserCircle} className="mr-2" />
+                    Profile
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="block w-full text-left px-4 py-2 text-darkBlue hover:bg-gray-100 hover:rounded-md flex items-center"
+                    onClick={handleLogoutClick}
+                  >
+                    <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+                    Logout
                   </button>
                 </li>
               </ul>
