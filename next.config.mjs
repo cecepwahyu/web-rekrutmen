6048d20490a37a19bfa7601894c83a2b92c09603
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2025-01-17 15:47:07 +0700

    feat: add local IP address to image domains for development

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    domains: ['localhost', 'api-rekrutmen:8080'],
+    domains: ['localhost', '192.168.4.79', 'api-rekrutmen'], // Add 'localhost' for local development
   },
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL, // Ensure your API URL is available
