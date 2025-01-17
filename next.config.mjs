d07f2d1d27c061199fe7e2e30509227054a2467d
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2025-01-17 15:42:51 +0700

    feat: update image domain for API integration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    domains: ['localhost', '192.168.4.79'], // Add 'localhost' for local development
+    domains: ['localhost', 'api-rekrutmen:8080'],
   },
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL, // Ensure your API URL is available
